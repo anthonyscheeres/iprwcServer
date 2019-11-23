52370ecf037f7cc4c00206f3cba64f846116fdc8
Anthony <info@anthonyscheeres.nl>
2019-11-23 22:08:36 +0100

    .

--- src/main/java/nl/iipsen2server/controlllers/ProductController.java
+++ src/main/java/nl/iipsen2server/controlllers/ProductController.java
@@ -5,12 +5,21 @@ import main.java.nl.iipsen2server.models.ProductModel;
 
 public class ProductController {
 	ProductDAO DAO= new ProductDAO();
-
+	AuthenticationController authenticationController = new AuthenticationController();
+	TokenController tokkenController = new TokenController();
+	
+	
 	public String handleGetAllProducts() throws Exception {
 	return DAO.showProducts();
 	}
 
-	public void handleCreateProduct(ProductModel u) throws Exception {
+	public void handleCreateProduct(ProductModel u, String token) throws Exception {
+		
+		long employeeId = Long.parseLong(tokkenController.tokenToUserId(token));
+		if (!authenticationController.hasSuperPermission(employeeId)) {
+			return;
+		}
+		
 		DAO.addProduct(u);
 		
 	}
--- src/main/java/nl/iipsen2server/resources/ProductResource.java
+++ src/main/java/nl/iipsen2server/resources/ProductResource.java
@@ -4,6 +4,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -38,10 +39,10 @@ public class ProductResource {
 	* 
 	*/
 	@POST
-	@Path("/create")
+	@Path("/{token}/create")
 	@Consumes(MediaType.APPLICATION_JSON)
-	public void createUserModel(ProductModel u ) throws Exception  {
-		 productController.handleCreateProduct(u);
+	public void createUserModel(ProductModel u, @PathParam("token") String token) throws Exception  {
+		 productController.handleCreateProduct(u, token);
 	}
 	
 }
